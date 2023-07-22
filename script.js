d404df0585d485001c0c19b6db9ac428e4a2de47
TheHuntsman4 <anikethvij464@gmail.com>
2023-07-22 21:53:53 +0530

    Initial commit Completed basic API handling and some styling

---
+++ script.js
@@ -0,0 +1,51 @@
+document.addEventListener("DOMContentLoaded", function(){
+    const greetingElement = document.getElementById("greetingMessage");
+    const Time = new Date().getHours();
+
+    let message;
+    if(Time>=0 && Time<12){
+        message="Good Morning";
+    }
+    else if(Time>=12 && Time < 18){
+        message="Good Afternoon";
+    }
+    else{
+        message="Good Evening"
+    }
+    greetingElement.innerHTML=message;
+});
+
+const APIKey = "0583365f139ec165ccba2b00c1ea7b11";
+const APIURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&";
+const weatherMain = document.getElementById("weather-main");
+const query = document.querySelector(".search input");
+const searchButton = document.querySelector(".search button");
+
+async function getWeather(city){
+
+  const response = await fetch(APIURL+"q="+city+`&appid=${APIKey}`);
+  var data = await response.json();
+
+  console.log(data);
+  console.log(query.value);
+
+  const weather = data.weather[0].main;
+  if(weather == "Clear"){
+    weatherMain.style.background="linear-gradient(129deg, #A1973B 25.87%, #00D2EF 100%)";
+
+  }
+  else{
+    weatherMain.style.backgroundColor="black";
+  }
+
+}
+
+searchButton.addEventListener("click",()=>{
+  getWeather(query.value);
+})
+
+query.addEventListener("keypress",function(event){
+  if(event.key=="Enter"){
+    getWeather(query.value);
+  }
+})
