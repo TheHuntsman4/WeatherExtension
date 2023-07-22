6674c69ce607470f2a6c428320e41060c0a30429
TheHuntsman4 <anikethvij464@gmail.com>
2023-07-23 01:15:37 +0530

    Feat Adds image files and makes constants for different components

--- script.js
+++ script.js
@@ -17,9 +17,13 @@ document.addEventListener("DOMContentLoaded", function(){
 
 const APIKey = "0583365f139ec165ccba2b00c1ea7b11";
 const APIURL = "https://api.openweathermap.org/data/2.5/weather?units=metric&";
+
 const weatherMain = document.getElementById("weather-main");
 const query = document.querySelector(".search input");
 const searchButton = document.querySelector(".search button");
+const cityNameElement = document.getElementById("city-name");
+const conditionElement = document.getElementById("weather-condition");
+const tempElement = document.getElementById("temp");
 
 async function getWeather(city){
 
@@ -28,7 +32,13 @@ async function getWeather(city){
 
   console.log(data);
   console.log(query.value);
+  console.log(data.name);
 
+  cityNameElement.innerHTML=data.name;
+  tempElement.innerHTML=data.main.temp;
+  conditionElement.innerHTML=data.weather[0].description;
+  
+  
   const weather = data.weather[0].main;
   if(weather == "Clear"){
     weatherMain.style.background="linear-gradient(129deg, #A1973B 25.87%, #00D2EF 100%)";
@@ -37,6 +47,8 @@ async function getWeather(city){
   else{
     weatherMain.style.backgroundColor="black";
   }
+
+
 
 }
 
